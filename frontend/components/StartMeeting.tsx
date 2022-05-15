7755f826dff3689223971954cb6a071d99e80c7a
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-05-16 01:37:28 +0900

    🚚 add: StartMeeting

---
+++ frontend/components/StartMeeting.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { StyleSheet, TextInput, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+type StartMeetingProps = {
+  name: string;
+  roomId: string;
+  setName: (name: string) => void;
+  setRoomId: (roomId: string) => void;
+};
+
+const StartMeeting: React.FC<StartMeetingProps> = ({ name, roomId, setName, setRoomId }) => {
+  return (
+    <View style={styles.startMeetingContainer}>
+      <View style={styles.info}>
+        <TextInput
+          style={styles.textInput}
+          value={name}
+          placeholder="Enter name"
+          placeholderTextColor="#767476"
+          onChangeText={(name) => setName(name)}
+        ></TextInput>
+      </View>
+      <View style={styles.info}>
+        <TextInput
+          style={styles.textInput}
+          value={roomId}
+          placeholder="Enter room id"
+          placeholderTextColor="#767476"
+          onChangeText={(room) => setRoomId(room)}
+        ></TextInput>
+      </View>
+
+      <View style={{ alignItems: "center" }}>
+        <TouchableOpacity style={styles.startMeetingButton} onPress={() => {}}>
+          <Text style={styles.startMeetingText}>Start Meeting</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default StartMeeting;
+
+const styles = StyleSheet.create({
+  startMeetingContainer: {},
+  info: {
+    width: "100%",
+    backgroundColor: "#373538",
+    height: 50,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: "#484648",
+    padding: 12,
+    justifyContent: "center",
+  },
+  textInput: {
+    color: "white",
+    fontSize: 18,
+  },
+  startMeetingText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  startMeetingButton: {
+    width: 350,
+    marginTop: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#0470DC",
+    height: 50,
+    borderRadius: 15,
+  },
+});
