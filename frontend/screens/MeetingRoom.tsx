3ba9913e5893f1efba2a48bd9980451149ec8ee1
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-05-30 01:07:52 +0900

    ✨ feat: add Camera

--- frontend/screens/MeetingRoom.tsx
+++ frontend/screens/MeetingRoom.tsx
@@ -1,21 +1,60 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text, Alert } from "react-native";
 import { io } from "socket.io-client";
 import type { Socket } from "socket.io-client";
 import { API_URL } from "@env";
+import { Camera, CameraType } from "expo-camera";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 import type { User } from "../../shared/types";
 
 import StartMeeting from "../components/StartMeeting";
+
+const menuItems = [
+  {
+    id: 1,
+    name: "microphone",
+    title: "Mute",
+    customColor: "#efefef",
+  },
+  {
+    id: 2,
+    name: "video-camera",
+    title: "Stop Video",
+  },
+  {
+    id: 3,
+    name: "upload",
+    title: "Share Content",
+  },
+  {
+    id: 4,
+    name: "group",
+    title: "Participants",
+  },
+];
 
 let socket: Socket;
 
 const MeetingRoom: React.FC = () => {
   const [name, setName] = useState("");
   const [roomId, setRoomId] = useState("");
-  const [activeUsers, setActiveUsers] = useState<User[]>([]);
+  const [activeUsers, setActiveUsers] = useState<User[]>(["Naz", "Joey"]);
+  const [startCamera, setStartCamera] = useState(false);
+
+  const __startCamera = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    if (status === "granted") {
+      setStartCamera(true);
+    } else {
+      Alert.alert("Camera permission denied");
+    }
+  };
 
   const joinRoom = () => {
+    __startCamera();
     socket.emit("join-room", {
       roomId,
       userName: name,
@@ -43,7 +82,33 @@ const MeetingRoom: React.FC = () => {
 
   return (
     <View style={styles.container}>
-      <StartMeeting {...props} />
+      {startCamera ? (
+        <SafeAreaView style={{ flex: 1 }}>
+          <View style={styles.activeUsersContainer}>
+            <View style={styles.cameraContainer}>
+              <Camera
+                type={CameraType.front}
+                style={{ width: activeUsers.length === 0 ? "100%" : 200, height: activeUsers.length === 0 ? 600 : 200 }}
+              ></Camera>
+              {activeUsers.map((user, index) => (
+                <View key={index} style={styles.activeUserContainer}>
+                  <Text style={{ color: "white " }}>{user}</Text>
+                </View>
+              ))}
+            </View>
+          </View>
+          <View style={styles.menu}>
+            {menuItems.map((item) => (
+              <TouchableOpacity key={item.id} style={styles.tile}>
+                <FontAwesome name={item.name} size={24} color={item.customColor} />
+                <Text style={styles.textTile}>{item.title}</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </SafeAreaView>
+      ) : (
+        <StartMeeting {...props} />
+      )}
     </View>
   );
 };
@@ -54,5 +119,40 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#1c1c1c",
     flex: 1,
+  },
+  tile: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50,
+    marginTop: 15,
+  },
+  textTile: {
+    color: "white",
+    marginTop: 10,
+  },
+  menu: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  cameraContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  activeUsersContainer: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "black",
+  },
+  activeUserContainer: {
+    borderColor: "gray",
+    borderWidth: 1,
+    width: 200,
+    height: 200,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
